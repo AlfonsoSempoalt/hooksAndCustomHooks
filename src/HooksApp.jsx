b0d1d2b02696a133643ff1baf753a243d49d8f0f
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-07-31 23:47:40 -0600

    create <hooksApp /> and bootstrap added

---
+++ src/HooksApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const HooksApp = props => {
+    return (
+        <>
+         <h1>Hello world</h1>   
+        </>
+    )
+}
+
+HooksApp.propTypes = {
+
+}
+
+export default HooksApp
